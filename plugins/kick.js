54a592c416e2ba331fc617addcb93060af80b600
Duxlin <96833294+Duxlin@users.noreply.github.com>
2022-05-26 01:16:13 +0100

    Initial commit

---
+++ plugins/kick.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn }) => {
+  if (m.quoted) {
+    await conn.groupRemove(m.chat, [m.quoted.sender])
+    conn.reply(conn.user.jid, `@${m.sender.split`@`[0]} remove @${m.quoted.sender.split`@`[0]}`, m)
+  }
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+}
+handler.help = ['remove'].map(v => v + ' @user')
+handler.tags = ['admin']
+handler.command = /^(remove|\-)$/i
+
+handler.owner = false
+handler.rowner = true
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = true
+handler.botAdmin = true
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
